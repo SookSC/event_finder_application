bb9bf605760c3f9f9210314e4855aafa30da143f
Sookeong Cho <skcho@uwaterloo.ca>
2025-03-24 20:47:36 -0400

    Add Retrofit instance for PredictHQ API

---
+++ app/src/main/java/com/example/eventFinder/network/RetrofitInstance.kt
@@ -0,0 +1,15 @@
+package com.example.eventFinder.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://api.predicthq.com/v1/events/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}

59c3e662aa92924329445da529ccae3424270c8f
Sookeong Cho <skcho@uwaterloo.ca>
2025-03-25 21:48:59 -0400

    Resolve constraint error in card_event_item.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.constraintlayout.v221)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 

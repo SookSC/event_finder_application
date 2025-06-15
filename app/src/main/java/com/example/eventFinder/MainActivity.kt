62eea86ba920b42d7c3cbe794921db99f54b7895
Sookeong Cho <skcho@uwaterloo.ca>
2025-06-14 20:54:38 -0400

    Delete obsolete navigation bar initialization

--- app/src/main/java/com/example/eventFinder/MainActivity.kt
+++ app/src/main/java/com/example/eventFinder/MainActivity.kt
@@ -18,35 +18,5 @@ class MainActivity : AppCompatActivity() {
         val navController: NavController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
-
-
-//        val navController = findNavController(R.id.nav_host_fragment)
-
-        // Setup navbar (all bottom 3 options seem to work)
-        // Option1
-//        bottomNavigationView.setupWithNavController(navController)
-
-        // Option2
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-
-        //Option3
-//        bottomNavigationView.setOnItemSelectedListener { item ->
-//            when(item.itemId) {
-//                R.id.nav_home -> {
-//                    navController.navigate(R.id.nav_home)
-//                    true
-//                }
-//                R.id.nav_events -> {
-//                    navController.navigate(R.id.nav_events)
-//                    true
-//                }
-//                R.id.nav_profile -> {
-//                    navController.navigate(R.id.nav_profile)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-
     }
 }

11921e0f85b49ad4a5227b4e5a8735dd1c1f2e22
Sookeong Cho <skcho@uwaterloo.ca>
2025-06-17 19:23:06 -0400

    Remove duplicate intent-filter from MainActivity and update minSdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sookeongcho.eventHub"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

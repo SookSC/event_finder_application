f2b4e56837eaecfeb08b6047b36d62a50023c48f
Sookeong Cho <skcho@uwaterloo.ca>
2025-06-17 19:49:26 -0400

    Load event image from URL using Glide in adapter

--- app/src/main/java/com/sookeongcho/eventHub/ui/adapter/TicketmasterEventAdapter.kt
+++ app/src/main/java/com/sookeongcho/eventHub/ui/adapter/TicketmasterEventAdapter.kt
@@ -3,8 +3,10 @@ package com.sookeongcho.eventHub.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sookeongcho.eventHub.R
 import com.sookeongcho.eventHub.data.model.TicketmasterEventItem
 import java.time.OffsetDateTime
@@ -18,6 +20,7 @@ class TicketmasterEventAdapter(private var eventDataSet: List<TicketmasterEventI
         val eventLocation: TextView = view.findViewById(R.id.tvEventLocation)
         val eventCategory: TextView = view.findViewById(R.id.tvEventCategory)
         val eventStart: TextView = view.findViewById(R.id.tvEventDateTime)
+        val eventImage: ImageView = view.findViewById(R.id.ivEventImage)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -35,6 +38,12 @@ class TicketmasterEventAdapter(private var eventDataSet: List<TicketmasterEventI
         viewHolder.eventLocation.text = location?.city?.name
         viewHolder.eventCategory.text = event.classifications?.first()?.segment?.name
         viewHolder.eventStart.text = parsedTime.format(formatter)
+
+        Glide.with(viewHolder.itemView.context)
+            .load(event.images?.first()?.url)
+            .placeholder(R.drawable.ic_eh_logo)
+            .error(R.drawable.ic_error_exclamation)
+            .into(viewHolder.eventImage)
     }
 
     override fun getItemCount() = eventDataSet.size
